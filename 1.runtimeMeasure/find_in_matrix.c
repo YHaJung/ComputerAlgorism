392675c6ba51aac2494fb4a6c39df4ecc9feeab1
YHaJung <49065638+YHaJung@users.noreply.github.com>
2021-04-19 12:45:59 +0900

    find in matrix my code

--- 1.runtimeMeasure/find_in_matrix.c
+++ 1.runtimeMeasure/find_in_matrix.c
@@ -1,34 +1,48 @@
-//pseudo code
-/*
-Alg findRow(A, x)
-input matrix A of n
-	  element x
-output index i such that A[i]=x
-		or -1 if there is no A[i] which is same with x
+#include <stdio.h>
+#include <stdlib.h>
 
-i<-0
-while(i<n)
-	if(R[i]=x)
-		return i
-	else
-		i <- i+1
-return -1
+#define MAXSIZE 5
 
-------------------------------------
-Alg findMatrix(A, n)
-input matrix A of nxn
-	  element x
-output location of x in A
-		or failure message if there is no element x in A
+int findRow(int x, int A[], int n) {
+	
+	int i = 0;
+	while (i < n) {
+		if (A[i] == x) {
+			return i;
+		}
+		else {
+			
+			i++;
+		}
+	}
+	return -1;
+}
 
-r<-0
-while(r<n)
-	i <- findRow(A[i], x)
-	if(i >= 0)
-		write(r, ',', i)
-		return
-	else
-		r<-r+1
-write("not found")
-return
-*/
+int findMatrix(int x, int A[MAXSIZE][MAXSIZE], int n) {
+	
+	int r = 0;
+	int i;
+	while (r < n) {
+		i = findRow(x, A[r], n);
+		if (i >= 0) {
+			printf("(%d, %d)\n", r, i);
+			return;
+		}
+		else {
+			r++;
+		}
+	}
+	printf("not found");
+	return;
+}
+
+void main() {
+	int A[MAXSIZE][MAXSIZE] = {
+		{101,102,103,104,105},
+		{106,107,108,109,110},
+		{111,112,113, 114, 115},
+		{116, 117, 118, 119, 120},
+		{216, 217, 218, 29, 220}
+	};
+	findMatrix(108, A, 5);
+}
