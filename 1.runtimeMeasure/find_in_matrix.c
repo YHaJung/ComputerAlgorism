5374f06e0c270eae31b999edbcb6d003e107a78a
YHaJung <49065638+YHaJung@users.noreply.github.com>
2021-04-19 21:51:32 +0900

    update find_in_matrix - prof. ver.
    add random int, scanf, make array, print array

--- 1.runtimeMeasure/find_in_matrix.c
+++ 1.runtimeMeasure/find_in_matrix.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAXSIZE 5
+#define ROWS 8
+#define COLS 8
 
-int findRow(int x, int A[], int n) {
-	
-	int i = 0;
-	while (i < n) {
-		if (A[i] == x) {
-			return i;
-		}
-		else {
-			
-			i++;
-		}
+void makeArray(int A[][COLS]) {
+	for (int r = 0; r < ROWS; r++)
+		for (int c = 0; c < COLS; c++)
+			A[r][c] = rand() % 100; //0~99 숫자 나옴
+}
+
+void printArray(int A[][COLS]) {
+	for (int r = 0; r < ROWS; r++) {
+		for (int c = 0; c < COLS; c++)
+			printf("%2d ", A[r][c]); //%2d 2자리보다 작으면 공백으로 2자리 확보
+		printf("\n");
 	}
+	printf("\n");
+}
+
+int findRow(int A[], int key) {
+	for (int c = 0; c < COLS; c++)
+		if (A[c] == key)
+			return c;
 	return -1;
 }
 
-int findMatrix(int x, int A[MAXSIZE][MAXSIZE], int n) {
+int findMatrix(int A[][COLS], int key) {
 	
 	int r = 0;
-	int i;
-	while (r < n) {
-		i = findRow(x, A[r], n);
-		if (i >= 0) {
-			printf("(%d, %d)\n", r, i);
+	int index;
+	while (r < ROWS) {
+		index = findRow(A[r], key);
+		if (index >= 0) {
+			printf("(%d, %d)에서 %d 발견\n", r, index, key);
 			return;
 		}
 		else {
 			r++;
 		}
 	}
-	printf("not found");
+	printf("not found\n");
 	return;
 }
 
 void main() {
-	int A[MAXSIZE][MAXSIZE] = {
-		{101,102,103,104,105},
-		{106,107,108,109,110},
-		{111,112,113, 114, 115},
-		{116, 117, 118, 119, 120},
-		{216, 217, 218, 29, 220}
-	};
-	findMatrix(108, A, 5);
+	int A[ROWS][COLS];
+	srand(time(NULL));	//없으면, 계속 같은 난수 발생
+	makeArray(A);
+	printArray(A);
+
+	int key;
+	printf("Input a key value : ");
+	scanf_s("%d", &key);
+
+	findMatrix(A, key);
 }
