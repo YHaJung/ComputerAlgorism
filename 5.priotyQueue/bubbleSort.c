87db8a58fc85bbe52d18045d57d5a90df6e699d7
YHaJung <49065638+YHaJung@users.noreply.github.com>
2021-04-23 10:44:59 +0900

    change to step by step print

--- 5.priotyQueue/bubbleSort.c
+++ 5.priotyQueue/bubbleSort.c
@@ -5,16 +5,6 @@
 #define SWAP(x,y,t) ((t)=(x), (x)=(y), (y)=(t))
 #define SIZE 15
 
-/*단계별 출력 제외하면, 교수님이 한 것과 완전히 일치*/
-void bubbleSort(int list[], int n) {
-	int temp;
-	for (int i = n; i > 1; i--) {
-		for (int j = 0; j < i-1; j++) {
-			if (list[j] > list[j + 1])
-				SWAP(list[j], list[j + 1], temp);
-		}
-	}
-}
 
 printArray(int arr[], int n) {
 	for (int i = 0; i < n; i++)
@@ -22,6 +12,22 @@ printArray(int arr[], int n) {
 	printf("\n");
 }
 
+/*단계별 출력 제외하면, 교수님이 한 것과 완전히 일치*/
+void bubbleSort(int list[], int n) {
+	printf("정렬할 원소 : ");
+	printArray(list, SIZE);
+
+	printf("<<<<버블정렬 수행>>>>\n");
+	int temp;
+	for (int i = n; i > 1; i--) {
+		for (int j = 0; j < i-1; j++) {
+			if (list[j] > list[j + 1])
+				SWAP(list[j], list[j + 1], temp);
+		}
+		printf("%d단계 : ", n - i + 1);
+		printArray(list, SIZE);
+	}
+}
 
 void main() {
 	int list[SIZE];
@@ -29,7 +35,7 @@ void main() {
 	for (int i = 0; i < SIZE; i++)
 		list[i] = rand() % 100;
 
-	printArray(list, SIZE);
+	
 	bubbleSort(list, SIZE);
-	printArray(list, SIZE);
+	
 }
